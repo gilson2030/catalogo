ef3e1c52942c78bfb127f891830f0a9df801e919
gilson2030 <fisicogilson@hotmail.com>
2025-06-29 18:52:21 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Substitua com as suas credenciais do Firebase:
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
