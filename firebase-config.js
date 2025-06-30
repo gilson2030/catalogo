56f0f59ad524bc9f919d862283132e8da27c0319
gilson2030 <fisicogilson@hotmail.com>
2025-06-30 10:06:43 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,17 @@
-// Substitua com as suas credenciais do Firebase:
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyDPMe4QYcTEGUOGnyBPuW5bGBLDWmoTMnI",
+  authDomain: "catalogo-c59c6.firebaseapp.com",
+  projectId: "catalogo-c59c6",
+  storageBucket: "catalogo-c59c6.firebasestorage.app",
+  messagingSenderId: "724697780828",
+  appId: "1:724697780828:web:1438cad544c49c85bacc99"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
